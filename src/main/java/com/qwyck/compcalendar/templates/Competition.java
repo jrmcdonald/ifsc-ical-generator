edd9a537a1fcf52dc293e519c56f2a9b6807fbb4
Jamie McDonald <jamie@qwyck.net>
2018-06-02 22:19:18 +0100

    Remove super() calls.

--- src/main/java/com/qwyck/compcalendar/templates/Competition.java
+++ src/main/java/com/qwyck/compcalendar/templates/Competition.java
@@ -7,111 +7,109 @@ import java.util.Date;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Competition {
 
-  @JsonProperty("cat_id")
-  private long cat;
+    @JsonProperty("cat_id")
+    private long cat;
 
-  @JsonProperty("date")
-  private Date startDate;
+    @JsonProperty("date")
+    private Date startDate;
 
-  @JsonProperty("date_end")
-  private Date endDate;
+    @JsonProperty("date_end")
+    private Date endDate;
 
-  private String name;
-  private String homepage;
-  private long duration;
+    private String name;
+    private String homepage;
+    private long duration;
 
-  public Competition() {
-    super();
-  }
+    public Competition() {}
 
-  /**
-   * @return the cat
-   */
-  public long getCat() {
-    return cat;
-  }
+    /**
+     * @return the cat
+     */
+    public long getCat() {
+        return cat;
+    }
 
-  /**
-   * @param cat the cat to set
-   */
-  public void setCat(long cat) {
-    this.cat = cat;
-  }
+    /**
+     * @param cat the cat to set
+     */
+    public void setCat(long cat) {
+        this.cat = cat;
+    }
 
-  /**
-   * @return the startDate
-   */
-  public Date getStartDate() {
-    return startDate;
-  }
+    /**
+     * @return the startDate
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
 
-  /**
-   * @param startDate the startDate to set
-   */
-  public void setStartDate(Date startDate) {
-    this.startDate = startDate;
-  }
+    /**
+     * @param startDate the startDate to set
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
 
-  /**
-   * @return the endDate
-   */
-  public Date getEndDate() {
-    return endDate;
-  }
+    /**
+     * @return the endDate
+     */
+    public Date getEndDate() {
+        return endDate;
+    }
 
-  /**
-   * @param endDate the endDate to set
-   */
-  public void setEndDate(Date endDate) {
-    this.endDate = endDate;
-  }
+    /**
+     * @param endDate the endDate to set
+     */
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 
-  /**
-   * @return the name
-   */
-  public String getName() {
-    return name;
-  }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
 
-  /**
-   * @param name the name to set
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  /**
-   * @return the homepage
-   */
-  public String getHomepage() {
-    return homepage;
-  }
+    /**
+     * @return the homepage
+     */
+    public String getHomepage() {
+        return homepage;
+    }
 
-  /**
-   * @param homepage the homepage to set
-   */
-  public void setHomepage(String homepage) {
-    this.homepage = homepage;
-  }
+    /**
+     * @param homepage the homepage to set
+     */
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
 
-  /**
-   * @return the duration
-   */
-  public long getDuration() {
-    return duration;
-  }
+    /**
+     * @return the duration
+     */
+    public long getDuration() {
+        return duration;
+    }
 
-  /**
-   * @param duration the duration to set
-   */
-  public void setDuration(long duration) {
-    this.duration = duration;
-  }
+    /**
+     * @param duration the duration to set
+     */
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
 
-  @Override
-  public String toString() {
-    return String.format(
-        "Competition{ cat=%d, name='%s', startDate=%tD, endDate=%tD, homepage=%s }", cat, name,
-        startDate, endDate, homepage);
-  }
+    @Override
+    public String toString() {
+        return String.format(
+                "Competition{ cat=%d, name='%s', startDate=%tD, endDate=%tD, homepage=%s }", cat,
+                name, startDate, endDate, homepage);
+    }
 }
--- src/main/java/com/qwyck/compcalendar/templates/Competitions.java
+++ src/main/java/com/qwyck/compcalendar/templates/Competitions.java
@@ -9,45 +9,43 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Competitions {
 
-  private ArrayList<Competition> competitions;
+    private ArrayList<Competition> competitions;
 
-  /**
-   * Default constructor
-   */
-  public Competitions() {
-    super();
-  }
+    /**
+     * Default constructor
+     */
+    public Competitions() {}
 
-  /**
-   * Construct a {@link Competitions} with an array list of {@link Competition} objects
-   * 
-   * @param competitions
-   * @return the competitions object
-   */
-  public Competitions(final ArrayList<Competition> competitions) {
-    super();
-    this.competitions = competitions;
-  }
-
-  /**
-   * @return the competitions
-   */
-  public ArrayList<Competition> getCompetitions() {
-    if (competitions == null) {
-      competitions = new ArrayList<Competition>();
+    /**
+     * Construct a {@link Competitions} with an array list of {@link Competition} objects
+     * 
+     * @param competitions
+     * @return the competitions object
+     */
+    public Competitions(final ArrayList<Competition> competitions) {
+        super();
+        this.competitions = competitions;
     }
-    return competitions;
-  }
 
-  /**
-   * @param competitions the competitions to set
-   */
-  public void setCompetitions(ArrayList<Competition> competitions) {
-    this.competitions = competitions;
-  }
+    /**
+     * @return the competitions
+     */
+    public ArrayList<Competition> getCompetitions() {
+        if (competitions == null) {
+            competitions = new ArrayList<Competition>();
+        }
+        return competitions;
+    }
 
-  @Override
-  public String toString() {
-    return "Competitions{competitions=" + competitions + "}";
-  }
+    /**
+     * @param competitions the competitions to set
+     */
+    public void setCompetitions(ArrayList<Competition> competitions) {
+        this.competitions = competitions;
+    }
+
+    @Override
+    public String toString() {
+        return "Competitions{competitions=" + competitions + "}";
+    }
 }
