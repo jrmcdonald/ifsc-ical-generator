07d20e655f6196f32e9736273bc33ba30a962de0
Jamie McDonald <jamie@qwyck.net>
2018-06-03 18:23:01 +0100

    Return iCal.

--- src/main/java/com/qwyck/compcalendar/controllers/IfscController.java
+++ src/main/java/com/qwyck/compcalendar/controllers/IfscController.java
@@ -1,11 +1,18 @@
 package com.qwyck.compcalendar.controllers;
 
+import java.util.List;
 import com.qwyck.compcalendar.services.IfscService;
+import com.qwyck.compcalendar.templates.Competition;
 import com.qwyck.compcalendar.templates.Competitions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import biweekly.Biweekly;
+import biweekly.ICalendar;
+import biweekly.component.VEvent;
 
 @RestController
 public class IfscController {
@@ -18,8 +25,41 @@ public class IfscController {
     }
 
     @RequestMapping(value = "/ifsc/json", method = RequestMethod.GET, produces = "application/json")
-    public Competitions getJson() {
-        return service.findAll();
+    public Competitions getJson(@RequestParam(value = "cats", required = false) List<String> cats) {
+        Competitions competitions = null;
+
+        if (!StringUtils.isEmpty(cats)) {
+            competitions = service.findByCategory(cats);
+        } else {
+            competitions = service.findAll();
+        }
+
+        return competitions;
     }
 
+    @RequestMapping(value = "/ifsc/ical", method = RequestMethod.GET, produces = "text/calendar")
+    public String getCal(@RequestParam(value = "cats", required = false) List<String> cats) {
+        Competitions competitions = null;
+
+        if (!StringUtils.isEmpty(cats)) {
+            competitions = service.findByCategory(cats);
+        } else {
+            competitions = service.findAll();
+        }
+
+        ICalendar cal = new ICalendar();
+
+        for (Competition competition : competitions.getCompetitions()) {
+            VEvent event = new VEvent();
+
+            event.setDateStart(competition.getStartDate(), false);
+            event.setDateEnd(competition.getEndDate(), false);
+            event.setSummary(competition.getName());
+            event.setDescription(competition.getHomepage());
+
+            cal.addEvent(event);
+        }
+
+        return Biweekly.write(cal).go();
+    }
 }
